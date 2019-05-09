989ef2d64a0c3816b9377d48cfb3ac060f2d7005
christian ortiz <cfortizp@unal.edu.co>
2019-05-09 10:36:28 -0500

    Vista Familiar

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from './components/app/App';
 import  Login  from './components/login/login';
 import Pagina from './components/profile/pagina';
 import  Regist  from './components/regist/Regist';
+import  Familiar  from './components/formulario/Familiar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import {BrowserRouter, Route, Redirect} from 'react-router-dom';
@@ -17,7 +18,7 @@ ReactDOM.render(
     <Pagina/>
 
     <Regist/>
-
+    <Familiar/>
     
   </div>,document.getElementById('root')
 );
