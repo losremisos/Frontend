268356d4874a8221a76c774349eafc18466737bc
cfpinedao <cfpinedao@unal.edu.co>
2019-05-30 09:00:30 -0500

    retoques al Store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
 import { App } from './components/app/App';
-import reducers from './JS/reducers';
+import store from './store';
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/collapse';
@@ -12,7 +11,6 @@ import 'bootstrap/js/src/collapse';
 import 'font-awesome/css/font-awesome.min.css';
 
 
-const store = createStore(reducers);
 
 ReactDOM.render(
 
