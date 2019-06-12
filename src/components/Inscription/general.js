c1147d94840610fc565d2d20e8b597fa407397dd
christian ortiz <cfortizp@unal.edu.co>
2019-06-11 21:52:24 -0500

    Update Admin Info

--- src/components/Inscription/general.js
+++ src/components/Inscription/general.js
@@ -10,10 +10,9 @@ import Working_Info from './Working_Info/Working_info';
 import Admin_Review from './Review_Admin/AdminReview';
 import './general.css';
 
-
-class Inscription extends Component {
-    render() {
-        var Admin;
+export class Inscription extends Component {
+  render() {
+    var Admin;
         var User;
         if(this.props.admin){
             User = "none";
@@ -21,164 +20,164 @@ class Inscription extends Component {
         }else{
             User = "block";
             Admin = "none";
-        }
-    return (
-      <div>
-        <div className="App-header App">
-        <div class="container">                    
-                <div class="col-md-12">   
-
-
-                    <a className="btn  mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_10" role="button" aria-expanded="false" aria-controls="collapse_10">Información Personal</a>
-                    <div class="collapse" id="collapse_10">
-
-                        <form>
-                            <div class="card card-body">
-                                <a className="btn text-left btn-style btn-block" data-toggle="collapse" href="#collapse_11" role="button" aria-expanded="false" aria-controls="collapse_11">Información Basíca</a>
-                                <div class="collapse" id="collapse_11">
-                                    <div class="card card-body">                                  
-                                        <BasicInfo/>
-                                    </div>
-                                </div> 
-                                <div style={{display:Admin}}>
-                                    <Admin_Review/>
-                                </div> 
-
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_12" role="button" aria-expanded="false" aria-controls="collapse_12">Información Adicional</a>
-                                <div class="collapse" id="collapse_12">
-                                    <div class="card card-body">                                  
-                                        <AdditionalInfo/>
-                                    </div>
-                                </div> 
-                                <div style={{display:Admin}}>
-                                    <Admin_Review/>
-                                </div>                  
-                                
-                                <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
-                                
-                                
-                                
-                            </div>    
-                               
-                        </form>
-
-                    </div>
-
-
-
-
-
-                    <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_20" role="button" aria-expanded="false" aria-controls="collapse_20">Información Academica</a>
-                    <div class="collapse" id="collapse_20">
-
-                        <form>
-                                <div class="card card-body">
-                                <a className="btn  text-left btn-style btn-block" data-toggle="collapse" href="#collapse_21" role="button" aria-expanded="false" aria-controls="collapse_21">Educación (Básica y/o Media)</a>
-                                <div class="collapse" id="collapse_21">
-                                    <div class="card card-body">                                  
-                                        <AcademicInfo/>
-                                    </div>
-                                </div> 
-                                <div style={{display:Admin}}>
-                                    <Admin_Review/>
-                                </div> 
-
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_22" role="button" aria-expanded="false" aria-controls="collapse_22">Educación Superior</a>
-                                <div class="collapse" id="collapse_22">
-                                    <div class="card card-body">                                  
-                                        <HigherEducationInfo/>
-                                    </div>
-                                </div> 
-                                <div style={{display:Admin}}>
-                                    <Admin_Review/>
-                                </div> 
-                                <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
-
-                            </div>  
-                        </form>
-
-                    </div>
-
-                    <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_30" role="button" aria-expanded="false" aria-controls="collapse_30">Información Laboral</a>
-
-                    <form>
-                        <div class="collapse" id="collapse_30">
-                            <div class="card card-body">               
-                                <Working_Info/>  
-                                <div style={{display:Admin}}>
-                                    <Admin_Review/>
-                                </div>                  
-                                <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
-                            </div>
-                            
-                        </div>
-                    </form>
-
-                    <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_40" role="button" aria-expanded="false" aria-controls="collapse_40">Información Familiar</a>
-                    <div class="collapse" id="collapse_40">
-
-                        <form>
-                            <div class="card card-body">               
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_41" role="button" aria-expanded="false" aria-controls="collapse_41">Información de la Madre</a>
-                                <div class="collapse" id="collapse_41">
-                                    <div class="card card-body">                                  
-                                        <FatherInfo/>
-                                    </div>
-                                    <div style={{display:Admin}}>
-                                        <Admin_Review/>
-                                    </div>  
-                                </div>
-                                
-
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_42" role="button" aria-expanded="false" aria-controls="collapse_42">Información del Padre</a>
-                                <div class="collapse" id="collapse_42">
-                                    <div class="card card-body">                                  
-                                        <FatherInfo/>
-                                    </div>
-                                    <div style={{display:Admin}}>
-                                        <Admin_Review/>
-                                    </div> 
-                                </div>
-                                 
-
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_43" role="button" aria-expanded="false" aria-controls="collapse_43">Información de los Hermanos</a>
-                                <div class="collapse" id="collapse_43">
-                                    <div class="card card-body">                                  
-                                        <SiblingInfo/>
-                                    </div>
-                                    <div style={{display:Admin}}>
-                                        <Admin_Review/>
-                                    </div> 
-                                </div>
-                                 
-
-                                <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_44" role="button" aria-expanded="false" aria-controls="collapse_44">Dependencia</a>
-                                <div class="collapse" id="collapse_44">
-                                    <div class="card card-body">                                  
-                                        <DependenceInfo/>
-                                    </div>
-                                    <div style={{display:Admin}}>
-                                        <Admin_Review/>
-                                    </div> 
-                                </div>
-                                
-
-
-
-                                <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
-                            </div> 
-
-                        </form> 
-                        
-
-                    </div>
-                    <button type="submit" className="btn btn-style-submit mt-5" style={{display:Admin}}>Enviar Correo</button>
-
-             </div>
+        }   
+        return (
+            <div>        
+              <div className="App-header App">
+              <div class="container">                    
+                      <div class="col-md-12">   
+      
+      
+                          <a className="btn  mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_10" role="button" aria-expanded="false" aria-controls="collapse_10">Información Personal</a>
+                          <div class="collapse" id="collapse_10">
+      
+                              <form>
+                                  <div class="card card-body">
+                                      <a className="btn text-left btn-style btn-block" data-toggle="collapse" href="#collapse_11" role="button" aria-expanded="false" aria-controls="collapse_11">Información Basíca</a>
+                                      <div class="collapse" id="collapse_11">
+                                          <div class="card card-body">                                  
+                                              <BasicInfo/>
+                                          </div>
+                                      </div> 
+                                      <div style={{display:Admin}}>
+                                          <Admin_Review/>
+                                      </div> 
+      
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_12" role="button" aria-expanded="false" aria-controls="collapse_12">Información Adicional</a>
+                                      <div class="collapse" id="collapse_12">
+                                          <div class="card card-body">                                  
+                                              <AdditionalInfo/>
+                                          </div>
+                                      </div> 
+                                      <div style={{display:Admin}}>
+                                          <Admin_Review/>
+                                      </div>                  
+                                      
+                                      <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
+                                      
+                                      
+                                      
+                                  </div>    
+                                     
+                              </form>
+      
+                          </div>
+      
+      
+      
+      
+      
+                          <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_20" role="button" aria-expanded="false" aria-controls="collapse_20">Información Academica</a>
+                          <div class="collapse" id="collapse_20">
+      
+                              <form>
+                                      <div class="card card-body">
+                                      <a className="btn  text-left btn-style btn-block" data-toggle="collapse" href="#collapse_21" role="button" aria-expanded="false" aria-controls="collapse_21">Educación (Básica y/o Media)</a>
+                                      <div class="collapse" id="collapse_21">
+                                          <div class="card card-body">                                  
+                                              <AcademicInfo/>
+                                          </div>
+                                      </div> 
+                                      <div style={{display:Admin}}>
+                                          <Admin_Review/>
+                                      </div> 
+      
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_22" role="button" aria-expanded="false" aria-controls="collapse_22">Educación Superior</a>
+                                      <div class="collapse" id="collapse_22">
+                                          <div class="card card-body">                                  
+                                              <BasicInfo/>
+                                          </div>
+                                      </div> 
+                                      <div style={{display:Admin}}>
+                                          <Admin_Review/>
+                                      </div> 
+                                      <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
+      
+                                  </div>  
+                              </form>
+      
+                          </div>
+      
+                          <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_30" role="button" aria-expanded="false" aria-controls="collapse_30">Información Laboral</a>
+      
+                          <form>
+                              <div class="collapse" id="collapse_30">
+                                  <div class="card card-body">               
+                                      <Working_Info/>  
+                                      <div style={{display:Admin}}>
+                                          <Admin_Review/>
+                                      </div>                  
+                                      <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
+                                  </div>
+                                  
+                              </div>
+                          </form>
+      
+                          <a className="btn mb-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_40" role="button" aria-expanded="false" aria-controls="collapse_40">Información Familiar</a>
+                          <div class="collapse" id="collapse_40">
+      
+                              <form>
+                                  <div class="card card-body">               
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_41" role="button" aria-expanded="false" aria-controls="collapse_41">Información de la Madre</a>
+                                      <div class="collapse" id="collapse_41">
+                                          <div class="card card-body">                                  
+                                              <FatherInfo/>
+                                          </div>
+                                          <div style={{display:Admin}}>
+                                              <Admin_Review/>
+                                          </div>  
+                                      </div>
+                                      
+      
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_42" role="button" aria-expanded="false" aria-controls="collapse_42">Información del Padre</a>
+                                      <div class="collapse" id="collapse_42">
+                                          <div class="card card-body">                                  
+                                              <FatherInfo/>
+                                          </div>
+                                          <div style={{display:Admin}}>
+                                              <Admin_Review/>
+                                          </div> 
+                                      </div>
+                                       
+      
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_43" role="button" aria-expanded="false" aria-controls="collapse_43">Información de los Hermanos</a>
+                                      <div class="collapse" id="collapse_43">
+                                          <div class="card card-body">                                  
+                                              <SiblingInfo/>
+                                          </div>
+                                          <div style={{display:Admin}}>
+                                              <Admin_Review/>
+                                          </div> 
+                                      </div>
+                                       
+      
+                                      <a className="btn  mt-1 text-left btn-style btn-block" data-toggle="collapse" href="#collapse_44" role="button" aria-expanded="false" aria-controls="collapse_44">Dependencia</a>
+                                      <div class="collapse" id="collapse_44">
+                                          <div class="card card-body">                                  
+                                              <DependenceInfo/>
+                                          </div>
+                                          <div style={{display:Admin}}>
+                                              <Admin_Review/>
+                                          </div> 
+                                      </div>
+                                      
+      
+      
+      
+                                      <button type="submit" className="btn btn-style-submit mt-5" style={{display:User}}>Guardar</button>
+                                  </div> 
+      
+                              </form> 
+                              
+      
+                          </div>
+                          <button type="submit" className="btn btn-style-submit mt-5" style={{display:Admin}}>Enviar Correo</button>
+      
+                   </div>
+                  </div>
+              </div>       
             </div>
-        </div>       
-      </div>
-    );
+          );
   }
 }
 /*export default Inscription;*/
--- src/index.js
+++ src/index.js
@@ -13,25 +13,5 @@ import 'font-awesome/css/font-awesome.min.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 
-ReactDOM.render(
-  
-  <div>
-    <Router>
-    <App/>
-    <div><Link to="/login/">Login</Link></div>
-    <div><Link to="/profile/">Profile</Link></div>
-    <div><Link to="/regist/">Regist</Link></div>
-    <div><Link to="/inscription/">Inscription</Link></div>
-    <div><Link to="/admininscription/">Admin</Link></div>
-    <Route path="/login/" component={Login}/>
-    <Route path="/profile/" component={Pagina}/>
-    <Route path="/regist/" component={Regist}/>
-    <Route path="/inscription/" component={() => <Inscription admin ={false} />} />
-    <Route path="/admininscription/" component={() => <Inscription admin ={true} />} />
-    </Router>
-  </div>,document.getElementById('root')
-);
-
 
 ReactDOM.render(<App/>,document.getElementById('root'));
-
