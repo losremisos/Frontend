cdfbcdffb52aee5ac493826f8e7a329724a5af49
meguerroa <meguerreroa@unal.edu.co>
2019-06-16 10:28:40 -0500

    Imagenes borradas en home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -29,41 +29,13 @@ export class Home extends Component {
                         <div className="col-md-12 services-main-box">
                             <div className="row">
                             
-                                <div className="col-md-4 services-box boxN1">
-                                    <h4>
-                                        <a id="TilteSeeMilitarySituation" className="services-link" href="/default">Consulte el estado de su situación militar</a>
-                                    </h4>
-                                    <p className="styleImg">
-                                        <a href="/defalut"><img src={SourceImg} alt="Consulta_Estado"/></a>
-                                    </p>
-                                    <ul>
-                                        <li>Seleccione el tipo de documento e ingrese el número del documento de identidad.</li>
-                                        <li>Seleccione el tipo de documento e ingrese el número del documento de identidad.</li>
-                                    </ul>                    
-                                    <p>
-                                    </p>  
-                                </div>
+                                
 
                                 <div className="col-md-4 styleImg2">
                                     <img src={SourceImg4} alt="Escudo_Ejercito"/>
                                 </div>
 
-                                <div className="col-md-4 services-box boxN6">
-                                    <h4>
-                                        <a id="ContactUs" className="services-link" href="/defalut">Necesita ayuda: Contáctenos</a>
-                                    </h4>
-                                    <p className="styleImg">
-                                        <a href="/defalut"><img src={SourceImg2} alt="Ayuda"/></a>
-                                    </p>
-                                    <ul>
-                                        <li>Contamos con un servicio en línea de apoyo al usuario y una línea telefónica a nivel nacional.</li>
-                                        <li>Sus inquietudes, comentarios y sugerencias nos permiten prestarle un mejor servicio.</li>
-                                    </ul>                    
-                                    <p>
-                                    </p>
-                                    <p>
-                                    </p>
-                                </div>
+                                
                             </div>
                         </div>
                     </div> 
